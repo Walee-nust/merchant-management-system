90379c0baa735be5536f349b27e71e0d732c68ae
jamshaidkhalid <jamshaidkhalid32@gmail.com>
2023-02-16 09:07:12 +0500

    sign out 😎

--- client/src/app/signin/signin.component.ts
+++ client/src/app/signin/signin.component.ts
@@ -39,7 +39,7 @@ export class SigninComponent implements OnInit {
   ngOnInit() {
     this.signinForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+      password: new FormControl('', [Validators.required, Validators.minLength(2)])
     });
   }
 
